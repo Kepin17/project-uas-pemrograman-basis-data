f8d9f46f611e5cdb830e5dd8314afcfc300243ca
Kepin17 <servicenya4@gmail.com>
2024-11-24 15:13:38 +0700

    add: tailwind css and fontawesome cdn, create : nav layout , main layout and index.php

---
+++ components/fragments/navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar bg-[#15919B] w-full h-20 h-20 flex items-center justify-between p-8">
+        <div class="navbar-brand flex items-center">
+          <a class="navbar-item text-2xl font-bold flex items-center gap-3 text-[#213A58]" href="index.php">
+          <i class="fa-solid fa-book"></i>
+            <h1>OneBooks</h1>
+          </a>
+        </div>
+
+        <div class="items-wrapper flex items-center gap-8">
+          <ul class="navbar-menu flex items-center gap-7 text-[#213A58] font-bold">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="index.php">About</a></li>
+            <li><a href="index.php">Contact</a></li>
+            <li><a href="index.php">literacy Zone</a></li>
+          </ul>
+
+          <div class="cta-wrapper flex items-center gap-8">
+            <a href="" class="cta-btn w-24 h-8 text-[#213A58] bg-[#80EE98] flex justify-center items-center font-bold rounded-md">
+              <button>Login</button>
+            </a>
+            <a href="" class="cta-btn w-24 h-8 text-[#213A58] bg-[#80EE98] flex justify-center items-center font-bold rounded-md">
+              <button>Register</button>
+            </a>
+          </div>
+        </div>
+    </nav>
