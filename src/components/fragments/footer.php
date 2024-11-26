0b7b0ee1f661a885526014dd6a715da7442c1676
Kepin17 <servicenya4@gmail.com>
2024-11-27 01:11:28 +0700

    create footer section

--- src/components/fragments/footer.php
+++ src/components/fragments/footer.php
@@ -1,3 +1,31 @@
-<footer class="bg-[#0C6478] w-full h-20 flex items-center justify-center">
-  
+<footer class="bg-[#0C6478] w-full h-[16rem] flex gap-10 p-5">
+  <div class="left-textbox w-1/2 flex flex-col gap-5 ">
+    <h1 class="font-roboto text-2xl font-bold text-[#FFF4B7]">Onebook. Inc</h1>
+    <p class="font-roboto text-[#FFF4B7] opacity-80">OneBook is your gateway to a world of knowledge and stories. As a digital library platform, we offer an extensive collection of books across various genres, accessible anytime and anywhere. Join our community of book lovers and explore the endless possibilities of learning and imagination with OneBook</p>
+    <small class="font-roboto text-[#FFF4B7] opacity-80">&copy; 2024 OneBook. All rights reserved</small>
+  </div>
+  <div class="center-textbox w-1/2 flex flex-col gap-2">
+    <h1 class="font-roboto text-2xl font-bold text-[#FFF4B7]">Quick Links</h1>
+    <ul class="font-roboto text-[#FFF4B7] opacity-80">
+      <li><a href="">Home</a></li>
+      <li><a href="">About</a></li>
+      <li><a href="">Contact</a></li>
+      <li><a href="">Literacy Zone</a></li>
+    </ul>
+  </div>
+  <div class="right-textbox w-1/2 flex flex-col gap-5">
+    <h1 class="font-roboto text-2xl font-bold text-[#FFF4B7]">Contact Us</h1>
+    <div class="sosmed-wrap text-xl flex gap-4 text-[#FFF4B7]">
+      <a href="" class="sosmed-icon"><i class="fa-brands fa-facebook"></i></a>
+      <a href="" class="sosmed-icon"><i class="fa-brands fa-twitter"></i></a>
+      <a href="" class="sosmed-icon"><i class="fa-brands fa-instagram"></i></a>
+      <a href="" class="sosmed-icon"><i class="fa-brands fa-tiktok"></i></a>
+      <a href="" class="sosmed-icon"><i class="fa-brands fa-youtube"></i></a>
+    </div>
+
+    <div class="subs-email flex flex-col">
+      <input type="email" placeholder="Enter your email" class="p-2 rounded-md focus:outline-none">
+      <button class="cta-btn p-2 my-5 h-10 w-32 text-[#213A58] bg-[#80EE98] flex justify-center items-center font-bold rounded-md">Subscribe</button>
+    </div>
+  </div>
 </footer>
